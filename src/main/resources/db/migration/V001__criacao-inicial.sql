4758c433bef118460a3883a47bd88be49fcdbcb2
Felipe Soares <soares.felipe200@gmail.com>
2024-03-12 11:58:32 -0300

    refactor: remocao de campo desnecessario criacao inicial

--- src/main/resources/db/migration/V001__criacao-inicial.sql
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -1,1 +1,1 @@
-create table cliente (id integer not null auto_increment, dat_lan datetime(6), email varchar(255) not null, nome varchar(255) not null, obs varchar(255), senha varchar(255) not null, primary key (id)) engine=InnoDB;
+create table cliente (id integer not null auto_increment, dat_lan datetime(6), email varchar(255) not null, nome varchar(255) not null, senha varchar(255) not null, primary key (id)) engine=InnoDB;
