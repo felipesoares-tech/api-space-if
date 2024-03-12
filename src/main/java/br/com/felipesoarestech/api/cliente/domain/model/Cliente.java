6009d5c113b546f74e2efcb3749ed854965f7b32
Felipe Soares <soares.felipe200@gmail.com>
2024-03-12 11:46:01 -0300

    refactor: removendo atributo desenecessario, e adicionando validacao de email

--- src/main/java/br/com/felipesoarestech/api/cliente/domain/model/Cliente.java
+++ src/main/java/br/com/felipesoarestech/api/cliente/domain/model/Cliente.java
@@ -1,6 +1,7 @@
 package br.com.felipesoarestech.api.cliente.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -13,13 +14,12 @@ public class Cliente {
     private Integer id;
     @NotNull(message = "nome é obrigatório")
     private String nome;
+    @Email
     @NotNull(message = "email é obrigatório")
     private String email;
-
     @NotNull(message = "senha é obrigatória")
     private String senha;
     LocalDateTime datLan;
-    private String obs;
 
     @PrePersist
     public void prePersist() {
