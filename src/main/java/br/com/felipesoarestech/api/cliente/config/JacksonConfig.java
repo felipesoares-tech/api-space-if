c0d52bf6d7203424df355ffee4ce342cb1cf8477
Felipe Soares <soares.felipe200@gmail.com>
2024-07-27 10:34:24 -0300

    feat: data

--- src/main/java/br/com/felipesoarestech/api/cliente/config/JacksonConfig.java
+++ src/main/java/br/com/felipesoarestech/api/cliente/config/JacksonConfig.java
@@ -3,17 +3,17 @@ package br.com.felipesoarestech.api.cliente.config;
 import br.com.felipesoarestech.api.cliente.domain.util.ByteArrayDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
+
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(byte[].class, new ByteArrayDeserializer());
-        objectMapper.registerModule(module);
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
 }
