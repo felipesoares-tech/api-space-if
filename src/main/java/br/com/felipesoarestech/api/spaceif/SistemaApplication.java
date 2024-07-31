351cc5512082021ee389291b4c9342d0d3087e96
Felipe Soares <soares.felipe200@gmail.com>
2024-07-30 23:12:39 -0300

    feat: removendo local properties

--- src/main/java/br/com/felipesoarestech/api/spaceif/SistemaApplication.java
+++ src/main/java/br/com/felipesoarestech/api/spaceif/SistemaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:env/local.properties")
 public class SistemaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaApplication.class, args);
